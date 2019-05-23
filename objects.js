0ba89193b321836d2615e29cac5096a18b8651de
communal-principle-3181 <aellis19@outlook.com>
2019-05-23 20:13:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist,artistName, songTitle)
 
 function removeFromPlaylist(playlist, artistName)
 {
-  Object.assign({},playlist,{artistName});
+  delete playlist.artistName;
   
   return playlist;
 }
