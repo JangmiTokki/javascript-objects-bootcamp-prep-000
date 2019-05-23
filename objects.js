c09de5bb9e948e82caab6469b58c8f8200f68b2f
communal-principle-3181 <aellis19@outlook.com>
2019-05-23 20:21:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 
 function updatePlaylist(playlist,artistName, songTitle)
 {
-  Object.assign({},playlist,{[artistName]:songTitle});
+  Object.assign({},artistName, songTitle);
   //playlist[artistName] = songTitle;
 }
 
